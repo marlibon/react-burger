43d481359853181b4289000c7202ca6012dd3539
Marat Zagriev <marlic@list.ru>
2023-08-28 23:22:30 +0500

    feat: fetch data

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import logo from '../../images/logo.svg'
 import styles from './app.module.css'
 import AppHeader from '../app-header/app-header'
 import BurgerIngredients from '../burger-ingredients/burger-ingredients'
 import BurgerConstructor from '../burger-constructor/burger-constructor'
-import {data} from '../../utils/data'
+import {loadAllCards} from '../../utils/fetch'
 
 function App() {
+  const [data, setData] = useState([])
+  const [isLoading, setLoading] = useState(true)
+
+  useEffect(() => {
+    loadAllCards()
+      .then(({data}) => setData(data))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false))
+  }, [])
+
+  if (isLoading)
+    return (
+      <div className={styles.page}>
+        <h1>Loading...</h1>
+      </div>
+    )
   return (
     <div className={styles.page}>
       <AppHeader />
